5271da093377e3b41b8270fc9e6ebc0f89637d2c
TheHitMan7 <krtik.vrma@gmail.com>
2022-03-20 15:01:56 +0530

    Initial AOSP build system
    Signed-off-by: TheHitMan7 <krtik.vrma@gmail.com>

---
+++ core/prebuilt.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2022 TheHitMan7
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+# Set non-privileged app source
+ifeq (APPS,$(LOCAL_MODULE_CLASS))
+TARGET_MODULE := app
+endif
+
+# Set privileged app source
+ifeq (true,$(LOCAL_PRIVILEGED_MODULE))
+TARGET_MODULE := priv-app
+endif
+
+# Set overlay app source
+ifeq (true,$(LOCAL_OVERLAY_MODULE))
+TARGET_MODULE := overlay
+endif
+
+LOCAL_SRC_FILES := $(GAPPS_COMMON_SOURCE)/$(TARGET_MODULE)/$(LOCAL_PACKAGE_NAME)/$(LOCAL_PACKAGE_NAME).apk
---
+++ gapps.mk
@@ -0,0 +1,47 @@
+#
+# Copyright (C) 2022 TheHitMan7
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+# Common source
+GAPPS_COMMON_SOURCE := vendor/gapps/sources
+
+# APK source
+APK_PREBUILT := vendor/gapps/core/prebuilt.mk
+
+# Common modules
+PRODUCT_PACKAGES += \
+    AppleNLPBackend \
+    AuroraServices \
+    DejaVuNLPBackend \
+    DroidGuard \
+    Extension \
+    FossDroid \
+    LocalGSMNLPBackend \
+    LocalWiFiNLPBackend \
+    MicroGGMSCore \
+    MicroGGSFProxy \
+    MozillaUnifiedNLPBackend \
+    NominatimNLPBackend \
+    Phonesky
+
+# Common system files
+PRODUCT_COPY_FILES += \
+    vendor/gapps/sources/etc/default-permissions/default-permissions.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/default-permissions/default-permissions.xml
+
+PRODUCT_COPY_FILES += \
+    vendor/gapps/sources/etc/permissions/privapp-permissions-microg.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-google.xml
+
+PRODUCT_COPY_FILES += \
+    vendor/gapps/sources/etc/sysconfig/microg.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/google.xml
